3e5924cb34559296115e5025b5f53fb98e72b058
Tensor <narutokelechi@gmail.com>
2024-07-24 16:37:46 +0100

    modify: script function calling and add starter files

--- project_setup.sh
+++ project_setup.sh
@@ -2,14 +2,20 @@
 
 ml_project() {
 	read -rp 'project name? => ' project
-	mkdir project
+	mkdir $project
 	echo Created "$project" in "$PWD"
-	cd project || exit
+	cd $project || exit
+	cd ..
 	touch README.md inference.py .gitignore .env
-	cd project || exit
+	cd $project || exit
 	mkdir model assets
+	cd model || exit
+	touch model.py dataloader.py train.py utils.py __init__.py
 	cd .. || exit
 	lsd
+	cd .. || exit
 }
+
+ml_project
 
 echo 'Project successfully created'
